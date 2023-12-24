715bf9a934a410fded2da72de4509406185044d1
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-24 17:59:43 +0500

    datetime topic

---
+++ datetime.js
@@ -0,0 +1,10 @@
+const date = new Date()
+console.log(`toDateString ${date.toDateString()}`);
+console.log(`toISOString ${date.toISOString()}`);
+console.log(`toLocaleDateString ${date.toLocaleDateString()}`);
+console.log(`toLocaleString ${date.toLocaleString()}`);
+console.log(`toTimeString ${date.toTimeString()}`);
+console.log(`toJSON ${date.toJSON()}`);
+console.log(`toString ${date.toString()}`);
+console.log(`toUTCString ${date.toUTCString()}`);
+
