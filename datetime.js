a0779781a9e8891cdfeb9bd8436ef1d488f51e4d
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-25 18:20:41 +0500

    switch cases added

--- datetime.js
+++ datetime.js
@@ -52,3 +52,26 @@ if (someday > today) {
   text = "Today is after January 14, 2100.";
 }
 console.log(text);
+switch (new Date().getDay()) {
+  default:
+    text = "Looking forward to the Weekend";
+    break;
+  case 6:
+    text = "Today is Saturday";
+    break;
+  case 0:
+    text = "Today is Sunday";
+}
+switch (new Date().getDay()) {
+  case 4:
+  case 5:
+    text = "Soon it is Weekend";
+    break;
+  case 0:
+  case 6:
+    text = "It is Weekend";
+    break;
+  default:
+    text = "Looking forward to the Weekend";
+}
+console.log(text);
