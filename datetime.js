a08a0cd2d05ed7751290c4df91f5047df6ffa8d1
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-25 16:31:23 +0500

    learning setters

--- datetime.js
+++ datetime.js
@@ -35,6 +35,20 @@ console.log(fullMonth.getMonth());
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const d = new Date("2021-03-25");
-let month = months[d.getMonth()];
-let diff = new Date().getTimezoneOffset();
-console.log(diff);
+// let month = months[d.getMonth()];
+// let diff = new Date().getTimezoneOffset();
+// console.log(diff);
+console.log(d.toString());
+d.setUTCFullYear(2023, 6, 90)
+console.log(d.toString());
+
+const today = new Date();
+const someday = new Date();
+someday.setFullYear(2100, 0, 14);
+
+if (someday > today) {
+  text = "Today is before January 14, 2100.";
+} else {
+  text = "Today is after January 14, 2100.";
+}
+console.log(text);
