37d87b6e9b06084a521bd98c605272d9ddef8995
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-27 15:13:18 +0500

    working with json

---
+++ jsons.js
@@ -0,0 +1,5 @@
+let text = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+console.log(JSON.parse(text));
