8f0009dd13aa2de8f74f47fb2b2a11afe5eb916b
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-27 15:13:36 +0500

    manipulating properties

--- workingWithObjects.js
+++ workingWithObjects.js
@@ -1,70 +1,92 @@
 "use strict";
-var dog ={
-    name: "Bob",
-    speed: 39.5,
-    color: "red",
-    friends:["joey", 'michael'],
-    //agar probel bo'lsa qo'shtirnoq ishlatgan yaxshi
-    "tail long":'yes'
-}
-dog.name = "Anthony"
-//property qo'shish
-dog.bark = 'Woof'
-
-// console.log(dog)
-// delete dog.friends
-//console.log(dog)
-// console.log(dog.color)
-// console.log(dog["tail long"])
-// console.log(dog["friends"])
-
-var testObj = {
-    11: "Eleven",
-    12:"Twelve",
-    13: "Thirteen"
-}
-var eleven = 11;
-// console.log(testObj[eleven])
-// console.log(testObj[12])
-
-function checkObjProperty(checkProp)
 {
-    if(dog.hasOwnProperty(checkProp))
-    {
-        return dog[checkProp]
-    }
-    return "Not found"
+// var dog ={
+//     name: "Bob",
+//     speed: 39.5,
+//     color: "red",
+//     friends:["joey", 'michael'],
+//     //agar probel bo'lsa qo'shtirnoq ishlatgan yaxshi
+//     "tail long":'yes'
+// }
+// dog.name = "Anthony"
+// //property qo'shish
+// dog.bark = 'Woof'
+
+// // console.log(dog)
+// // delete dog.friends
+// //console.log(dog)
+// // console.log(dog.color)
+// // console.log(dog["tail long"])
+// // console.log(dog["friends"])
+
+// var testObj = {
+//     11: "Eleven",
+//     12:"Twelve",
+//     13: "Thirteen"
+// }
+// var eleven = 11;
+// // console.log(testObj[eleven])
+// // console.log(testObj[12])
+
+// function checkObjProperty(checkProp)
+// {
+//     if(dog.hasOwnProperty(checkProp))
+//     {
+//         return dog[checkProp]
+//     }
+//     return "Not found"
+// }
+
+// // console.log(checkObjProperty("color"))
+// // console.log(dog["friends"])
+
+// const ourStorage = {
+//   "desk": {
+//     "drawer": "stapler"
+//   },
+//   "cabinet": {
+//     "top drawer": { 
+//       "folder1": "a file",
+//       "folder2": "secrets"
+//     },
+//     "bottom drawer": "soda"
+//   }
+// };
+// var newStorage = ourStorage;
+// newStorage.desk.drawer = "pen"
+// var amazaonStorage = [
+//     ourStorage, newStorage
+// ]
+// var folder = ourStorage.cabinet["top drawer"]
+// //console.log(folder);
+// console.log(amazaonStorage[0].cabinet);
+// console.log();
+// // "use strict";
+// function myFunction() {
+//   //alert(this); // will alert "undefined"
+//   return this
+//   //console.log(this);
+// }
+
+// console.log(myFunction());
 }
 
-// console.log(checkObjProperty("color"))
-// console.log(dog["friends"])
-
-const ourStorage = {
-  "desk": {
-    "drawer": "stapler"
+var person = {
+  firstName: "John",
+  lastName : "Doe",
+  language : "NO",
+  get fname()
+  {
+    return this.firstName
   },
-  "cabinet": {
-    "top drawer": { 
-      "folder1": "a file",
-      "folder2": "secrets"
-    },
-    "bottom drawer": "soda"
+  set fname(value)
+  {
+    this.firstName = value
   }
 };
-var newStorage = ourStorage;
-newStorage.desk.drawer = "pen"
-var amazaonStorage = [
-    ourStorage, newStorage
-]
-var folder = ourStorage.cabinet["top drawer"]
-//console.log(folder);
-console.log(amazaonStorage[0].cabinet);
-console.log();
-// "use strict";
-function myFunction() {
-  //alert(this); // will alert "undefined"
-  return this
-  //console.log(this);
-}
 
-console.log(myFunction());
+// Change a Property:
+Object.defineProperty(person, "language", {
+  get : function() { return language },
+  set : function(value) { language = value.toUpperCase()}
+});
